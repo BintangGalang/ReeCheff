cecd1219e5494596f93b84e95fc0e455b258743f
BintangGalang <163822567+BintangGalang@users.noreply.github.com>
2024-06-29 10:20:32 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,16 @@ export default defineNuxtConfig({
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
+    
+  modules: ["@nuxt/ui", "nuxt-vuefire"],
+  vuefire: {
+    config: {
+    apiKey: "AIzaSyAbPtOxcC3KRjSgk6bpqWhcJ0hkqHOIIIQ",
+    authDomain: "project-ef619.firebaseapp.com",
+    projectId: "project-ef619",
+    storageBucket: "project-ef619.appspot.com",
+    messagingSenderId: "1088765065398",
+    appId: "1:1088765065398:web:4a21d06589f9610512ce17",
+    measurementId: "G-B0J9L7BGVP"
   },
 });
