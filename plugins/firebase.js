7880ece38a1dadd2b2f9efabe382a90b6528dbfa
ArifaJingga <163821391+ArifaJingga@users.noreply.github.com>
2024-06-29 12:09:35 +0800

    Update firebase.js

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,13 +1,20 @@
-import { getFirestore, collection } from "firebase/firestore";
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
-export default defineNuxtPlugin((nuxtApp) => {
-  const db = getFirestore(nuxtApp.$firebaseApp);
-  const modelsRef = collection(db, "todos");
+const firebaseConfig = {
+  apiKey: "AIzaSyAbPtOxcC3KRjSgk6bpqWhcJ0hkqHOIIIQ",
+  authDomain: "http://project-ef619.firebaseapp.com",
+  projectId: "project-ef619",
+  storageBucket: "http://project-ef619.appspot.com",
+  messagingSenderId: "1088765065398",
+  appId: "1:1088765065398:web:4a21d06589f9610512ce17",
+  measurementId: "G-B0J9L7BGVP"
+}
 
-  return {
-    provide: {
-      db,
-      modelsRef,
-    },
-  };
-});
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+const db = firebase.firestore()
+
+export { db }
