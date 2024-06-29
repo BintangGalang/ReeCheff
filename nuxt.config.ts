f0f60624383e3e2ac201d6b4e86b7d902495d8f5
Gede Raditya Putra <134995449+radityaptra@users.noreply.github.com>
2024-06-29 11:55:55 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
-export default {
-  css: ['@/assets/main.css'],
-  // other configurations
-}
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  routeRules: {
+    // prerender index route by default
+    '/': { prerender: true },
+    
+  modules: ["@nuxt/ui", "nuxt-vuefire"],
+  vuefire: {
+    config: {
+    apiKey: "AIzaSyAbPtOxcC3KRjSgk6bpqWhcJ0hkqHOIIIQ",
+    authDomain: "project-ef619.firebaseapp.com",
+    projectId: "project-ef619",
+    storageBucket: "project-ef619.appspot.com",
+    messagingSenderId: "1088765065398",
+    appId: "1:1088765065398:web:4a21d06589f9610512ce17",
+    measurementId: "G-B0J9L7BGVP"
+  },
+});
